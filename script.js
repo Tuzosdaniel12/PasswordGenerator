b42e1151df10e785e5afe09f17930ab2ce720bd8
Tuzosdaniel12 <danielsoledad@gmail.com>
2020-10-20 00:34:15 -0700

    adding character array to my object

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// global variables 
+var strongPassword;
+// object that i can store every requirement array
+var passwordRequirements = {
+numbers: ["1","2","3","4","5","6","7","8","9"],
+characters:["!","#","$","&","(",
+            "\'\'","\"\"","%",")","*",
+            "+",",","-",".",":",
+            ";","<","=",">","?",
+            "@","[","","]","^",
+            "_","`","{","|","}",
+            "~", "\/"],
+upperCase:[],
+lowerCase:[]
+}
+
+console.log(passwordRequirements);
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

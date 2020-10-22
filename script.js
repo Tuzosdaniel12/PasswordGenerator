e66dba826e81aa7d8dcebd5e8c00cf757b990700
Tuzosdaniel12 <danielsoledad@gmail.com>
2020-10-21 20:13:48 -0700

    adding commets

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ function writePassword() {
 function generatePassword(){
   var passwordLength= parseInt(prompt("How long would you like your password to be from " + minLength + " to " + maxLength + "?"));
     //Make sure that the user doesn't go over 128 character
-    while(passwordLength > 128 || passwordLength < 8){
-      passwordLength= parseInt(prompt("How long would you like your password to be from " + minLength + " to " + maxLength + "?"));
+    while(passwordLength > maxLength || passwordLength < minLength){
+      passwordLength= parseInt(prompt("Provide a correct length between " + minLength + " to " + maxLength + "?"));
     }
 
     //ask user if they like special characters,numbers,lowcase,and uppercase
@@ -45,29 +45,36 @@ function generatePassword(){
     //console.log(boolChar)
     //console.log(boolUpper)
     //console.log(boolLower)
+    var strongPassword = "";//store password here then return it 
 
-    var strongPassword = " ";//store password here then return it 
-    //put the password together while password is less then or equals to lenght of user choice 
-    while(strongPassword.length <= passwordLength){
-      //ramdom order of num,char,lower and upper case letters
-      var randomOrder = Math.floor(Math.random() * Object.keys(passwordRequirements).length);
-      switch (randomOrder) {
-        case 0://numbers
-        strongPassword = checkUserChoices(passwordRequirements.numbers, booleanNum, strongPassword);
-          break;
-        case 1://char
-        strongPassword = checkUserChoices(passwordRequirements.characters, booleanChar, strongPassword);
-          break;
-        case 2://upperCase
-        strongPassword = checkUserChoices(passwordRequirements.upperCase, booleanUpper,strongPassword);
-          break;
-        case 3://lowerCase
-        strongPassword = checkUserChoices(passwordRequirements.lowerCase, booleanLower, strongPassword);
-          break;  
-      }
-    }
-      //console.log(strongPassword); for de bug
+    //added this option ilearned from class activities, so I have one true at lease
+    if (booleanNum === false && booleanChar === false && booleanUpper === false && booleanLower === false){
+      alert("must pick an option, you must start again");
       return strongPassword;
+    }
+    else{ 
+      //put the password together while password is less then or equals to lenght of user choice 
+      while(strongPassword.length < passwordLength){
+        //random order of num,char,lower and upper case letters
+        var randomOrder = Math.floor(Math.random() * Object.keys(passwordRequirements).length);
+        switch (randomOrder) {
+          case 0://numbers
+          strongPassword = checkUserChoices(passwordRequirements.numbers, booleanNum, strongPassword);
+            break;
+          case 1://char
+          strongPassword = checkUserChoices(passwordRequirements.characters, booleanChar, strongPassword);
+            break;
+          case 2://upperCase
+          strongPassword = checkUserChoices(passwordRequirements.upperCase, booleanUpper,strongPassword);
+            break;
+          case 3://lowerCase
+          strongPassword = checkUserChoices(passwordRequirements.lowerCase, booleanLower, strongPassword);
+            break;  
+        }
+      }
+        //console.log(strongPassword); for de bug
+      return strongPassword;
+  }
   }
 
 //function ask user if they like num, uppercase , lowercase, char
@@ -80,10 +87,10 @@ function userChoiceBoolean(textHolder){
 function checkUserChoices(array, bool, strongPassword ){
   if (bool){
     var randOrder = Math.floor(Math.random() * array.length);//creates random number to pick random value with in array
-    console.log(strongPassword + array[randOrder]);
+    console.log(strongPassword + array[randOrder]);//de bug
     return strongPassword + array[randOrder]; //adds num,char, low or upper to strong password
   }
- else{
+ else{                          
    return strongPassword;
  }
 }
